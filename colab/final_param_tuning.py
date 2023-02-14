fb5d027e798a4dc498617ee0d5c762acacb92af0
EppChops <erik.00.berg@gmail.com>
2023-02-14 16:07:12 +0100

    Final testing

--- colab/final_param_tuning.py
+++ colab/final_param_tuning.py
@@ -14,23 +14,46 @@ from sklearn.preprocessing import RobustScaler
 from sklearn.feature_selection import SelectPercentile
 
 from sklearn.model_selection import train_test_split, cross_val_score
+import re
+
 
 def to_label(y):
-  return ['Anti-vaccine', 'Pro-vaccine'][int(y)]
+    return ['Anti-vaccine', 'Pro-vaccine'][int(y)]
 
-data = pd.read_csv("a3_train_round1.tsv", sep='\t', names=['Annotation', 'Comment'], header=None)
 
-data = data.sample(frac=1.0, random_state=0)
+data = pd.read_csv("a3_train_final.tsv", sep='\t', names=[
+                   'Annotation', 'Comment'], header=None)
 
+
+print(data)
+
+to_drop = []
+for i, sample in enumerate(data['Annotation']):
+    if (re.match('1/0|0/1|-1', str(sample))):
+        to_drop.append(i)
+    else:
+        if (re.match('1+', str(sample))):
+            data['Annotation'][i] = 1
+        else:
+            data['Annotation'][i] = 0
+
+data = data.drop(axis='index', index=to_drop)
+data.drop_duplicates(subset="Comment")
+print(data)
+
+data = data.sample(frac=1.0, random_state=0).reset_index(drop=True)
+print(data)
 X = data['Comment']
 
 Y = data['Annotation'].apply(to_label)
 
+print(X)
+print(Y)
 
 Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.2, random_state=0)
 
 
-steps = [('vect', TfidfVectorizer()), ('feature_select', SelectPercentile()), ('classif', SGDClassifier(max_iter=100, n_jobs=-1))]
+steps = [('vect', TfidfVectorizer()), ('feature_select', SelectPercentile()), ('classif', SGDClassifier(max_iter=400, n_jobs=-1))]
 
 pipeline = Pipeline(steps)
 
@@ -44,7 +67,7 @@ model_params = {
               #'feature_select__score_func': [f_classif, chi2],
               'classif__penalty': ['l2'],
              # 'classif__learning_rate': ['constant', 'optimal', 'invscaling', 'adaptive'],
-             'classif__loss': ['hinge', 'log_loss'],
+             'classif__loss': ['hinge', 'log'],
 #              'classif__eta0': [0.01, 0.001],
               'classif__alpha': np.linspace(0.1e-08, 0.001, num=10).tolist()
 }
@@ -66,5 +89,6 @@ print('Best score:', search.best_score_)
 #feature_scores = f_classif(Xtrain, Ytrain)[0]
 #for score, fname in sorted(zip(feature_scores, vectorizer.get_feature_names()), reverse=True)[:10]:
 #    print(fname, score)
+
 
 
